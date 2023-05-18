c93fd6ac69274469f3b18e0ec9976c1a4d25843b
leonellalevy <89606009+leonellalevy@users.noreply.github.com>
2023-05-17 23:23:30 -0400

    Update home.dart to get username

--- AppDevFinalProject/lib/home.dart
+++ AppDevFinalProject/lib/home.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import './globals.dart' as globals;
+import 'package:firebase_auth/firebase_auth.dart';
 
 //Home Page
 class HomePage extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
     return Center(
         child: Column(
-          children: [Text("It's nice to see you again Bob!")],
+          children: [Text("It's nice to see you again ${FirebaseAuth.instance.currentUser!.displayName}!")],
         ));
   }
 }
