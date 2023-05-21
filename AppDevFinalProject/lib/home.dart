45ed32001a36d6fd77e1e59a4bc4369db6f79d00
leonellalevy <89606009+leonellalevy@users.noreply.github.com>
2023-05-20 23:39:50 -0400

    Update home.dart

--- AppDevFinalProject/lib/home.dart
+++ AppDevFinalProject/lib/home.dart
@@ -1,16 +1,167 @@
 import 'package:flutter/material.dart';
-import './globals.dart' as globals;
 import 'package:firebase_auth/firebase_auth.dart';
+import 'Model.dart';
+import './globals.dart' as globals;
 
-//Home Page
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  ItemsList? selectedItemsList;
+  List<Item> items = [];
+  List<bool> checkedItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // Load the initial items list when the widget is initialized
+    loadItemsList(null); // Pass null as the initial value
+  }
+
+  Future<void> loadItemsList(ItemsList? itemsList) async {
+    if (itemsList != null) {
+      setState(() {
+        selectedItemsList = itemsList;
+        items = selectedItemsList!.itemList;
+        checkedItems = List<bool>.filled(items.length, false);
+      });
+    }
+  }
+
+  Future<List<ItemsList>> getAllItemsList() async {
+    return await globals.db.allItemsList();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-          children: [Text("It's nice to see you again ${FirebaseAuth.instance.currentUser!.displayName}!")],
-        ));
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "Welcome back, ${FirebaseAuth.instance.currentUser!.displayName}!",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            decoration:
+                BoxDecoration(shape: BoxShape.circle, color: globals.mainColor),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 20),
+            child: Text(
+              "Recent lists!",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.start,
+            ),
+            alignment: Alignment.topLeft,
+          ),
+          MyDropdown(onOptionSelected: loadItemsList),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index];
+                final isChecked = checkedItems[index];
+                return ListTile(
+                  leading: CircleAvatar(
+                    child: Text('${index + 1}'),
+                  ),
+                  title: Text(item.itemName),
+                  trailing: Checkbox(
+                    value: isChecked,
+                    onChanged: (bool? newValue) {
+                      setState(() {
+                        checkedItems[index] = newValue ??
+                            false; // Update the checked state for the item
+                      });
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyDropdown extends StatefulWidget {
+  final Function(ItemsList) onOptionSelected; // Accept a callback function
+
+  MyDropdown({required this.onOptionSelected});
+
+  @override
+  _MyDropdownState createState() => _MyDropdownState();
+}
+
+class _MyDropdownState extends State<MyDropdown> {
+  int selectedOptionIndex = 0;
+  List<ItemsList> dropdownOptions = [];
+
+  Future<List<ItemsList>> getAllItemsList() async {
+    return await globals.db.allItemsList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<ItemsList>>(
+      future: getAllItemsList(),
+      builder: (context, snapshot) {
+        try {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return DropdownButton<int>(
+              value: selectedOptionIndex,
+              items: [
+                DropdownMenuItem<int>(
+                  value: 0,
+                  child: Text('Loading...'),
+                ),
+              ],
+              onChanged: (int? newIndex) {
+                setState(() {
+                  selectedOptionIndex = newIndex!;
+                });
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            dropdownOptions = snapshot.data!;
+            return DropdownButton<int>(
+              value: selectedOptionIndex,
+              items: dropdownOptions.asMap().entries.map((entry) {
+                final index = entry.key;
+                final option = entry.value;
+                return DropdownMenuItem<int>(
+                  value: index,
+                  // Use the index as the value instead of duplicate values
+                  child: Text(option.itemListTitle),
+                );
+              }).toList(),
+              onChanged: (int? newIndex) {
+                setState(() {
+                  selectedOptionIndex = newIndex!;
+                });
+                widget.onOptionSelected(dropdownOptions[
+                    selectedOptionIndex]); // Call the callback function
+              },
+            );
+          }
+        } catch (e, stackTrace) {
+          return DropdownButton(items: null, onChanged: null);
+        }
+      },
+    );
   }
 }
